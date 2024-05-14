46fe4cafb9ada7266c37dcd7f8a945aa4e9dec8b
Michael Liu <mike666234@gmail.com>
2024-05-14 03:09:22 -0700

    Add gpt-4o (#585)
    * Add gpt-4o to chat.ts
    
    * Add gpt-4o to chat.ts
    
    (cherry picked from commit fbc47ebccb565644a3c2166f81dc3327c43301b5)

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -51,8 +51,8 @@ export interface Folder {
 }
 
 export type ModelOptions =
-  | 'gpt-3.5-turbo'
-  | 'gpt-4-turbo'
+  | 'gpt-4o'
+  | 'gpt-4o-2024-05-13'
   | 'gpt-4'
   | 'gpt-4-32k'
   | 'gpt-4-1106-preview'
